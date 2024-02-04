f230e0e229a745d054189ebae492aaa22159ff14
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-02-04 13:59:12 +0300

    complete assigment

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Task from "./Task";
+
+function TaskList({tasks, deletedItem}) {
+  return (
+    <div className="tasks">
+      {tasks.map((task, index) => (
+         <Task 
+         deletedItem={deletedItem}
+         key={index}
+         text={task.text}
+         category={task.category}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
